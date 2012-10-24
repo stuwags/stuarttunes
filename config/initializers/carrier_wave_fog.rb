5748873913c80c85f422256b5c94a5111cf4ce2b
Stuart Wagner <stuwags@gmail.com>
2012-10-24 10:25:55 -0400

    aws with heroku

--- config/initializers/carrier_wave_fog.rb
+++ config/initializers/carrier_wave_fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config| 
   config.fog_credentials = { 
     :provider               => 'AWS', 
-    :aws_access_key_id      => 'TODO: Replace with env vars', 
-    :aws_secret_access_key  => 'TODO: Replace with env vars', 
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], 
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
   } 
   config.fog_directory  = 'stuart_tunes' 
   config.fog_public     = false 
