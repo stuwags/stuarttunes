64bf5666e632949d50da54245b3da0a94ef7e752
Stuart Wagner <stuwags@gmail.com>
2012-10-25 13:13:56 -0400

    Add annotate.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,17 @@
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  price      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  audiofile  :string(255)
+#
+
 class Song < ActiveRecord::Base
   attr_accessible :name, :price, :audiofile
   mount_uploader :audiofile, AudiofileUploader
-
+  # has_and_belongs_to_many :artists
 end
