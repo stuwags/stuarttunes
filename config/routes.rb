be6d17aabf1a36185ed4d3c91ef35ef996294c35
Stuart Wagner <stuwags@gmail.com>
2012-10-25 10:24:32 -0400

    Replaced public/index with link to songs#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 StuartTunes::Application.routes.draw do
   resources :posts
   resources :songs
+  root :to => 'songs#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
