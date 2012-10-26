7a08f1e61ead3d3ff1c089ab7b79a3d32511f497
Stuart Wagner <stuwags@gmail.com>
2012-10-26 12:13:34 -0400

    Created first cut of model for album

---
+++ app/models/album.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  photo        :string(255)
+#  release_date :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Album < ActiveRecord::Base
+  attr_accessible :name, :photo, :release_date
+  mount_uploader :photo, AudiofileUploader
+end
+pho
