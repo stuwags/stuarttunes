42e976018679ed74107179fcdcd0e7e89682b50d
Stuart Wagner <stuwags@gmail.com>
2012-10-29 14:50:50 -0400

    Added authenticate admin to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  include ApplicationHelper
+
+  def authenticate_admin
+    if !current_user.admin?
+      redirect_to songs_path
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def authenticate_admin
-    if !current_user.admin?
-      redirect_to songs_path
-    end
-  end
+
 
 end
