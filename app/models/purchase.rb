eadecfeb0384666d2b4a1e03fd095e25d6b91f7b
Stuart Wagner <stuwags@gmail.com>
2012-11-02 00:45:25 -0400

    Refactored Purchases

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id      :integer          not null, primary key
+#  user_id :integer
+#  song_id :integer
+#  price   :decimal(, )
+#
+
 class Purchase < ActiveRecord::Base
   attr_accessible :song_id
   belongs_to :user
