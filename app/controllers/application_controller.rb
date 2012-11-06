00ad50c66ae4eec9b376ddab0a6f6ad313d24496
Stuart Wagner <stuwags@gmail.com>
2012-11-06 16:24:26 -0500

    Fixed bug with before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!, :except => [:home,:index]
+  before_filter :authenticate_user!
 
   def authenticate_admin
     if !current_user.admin?
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+
+  skip_before_filter :authenticate_user!
+
   def index
     @photos = []
     Album.all.each do |album|
