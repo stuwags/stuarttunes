3c728e20cda598552455935683432b981ca6fe6a
Stuart Wagner <stuwags@gmail.com>
2012-11-06 20:33:58 -0500

    Style changes to views

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,5 +3,5 @@ var effect = ["wave", "zipper", "curtain"];
 
 $(document).ready( function(){
   $('#slideshowHolder').jqFancyTransitions({ width: 800, height: 900, effect: effect[Math.floor(Math.random()*3)]
-    , stripDelay: .1, navigation: true, strips: 20 });
+    , stripDelay: .1, strips: 20 });
 });
